222c5dac6dc353999763e2f0b5dc36fd41727480
Prakhar Kumar <prakhar.meerut@gmail.com>
2023-07-02 13:58:56 +0530

    Added Formula Bar

--- src/FormulaActions/index.js
+++ src/FormulaActions/index.js
@@ -1,5 +1,13 @@
 import "./FormulaActions.css";
 
 export default function FormulaActions() {
-  return <div className="formula-container"></div>;
+  return (
+    <div className="formula-container">
+      <div className="formula-items formula-active-cell">B6</div>
+      <div className="formula-items formula-group">
+        <span className="fx">fx</span>
+        <input className="formula-input"></input>
+      </div>
+    </div>
+  );
 }
