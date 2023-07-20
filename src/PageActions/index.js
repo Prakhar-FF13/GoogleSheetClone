3c0ee8643a9482f757ed7af9a6791133e0449be1
Prakhar Kumar <prakhar.meerut@gmail.com>
2023-07-20 14:49:04 +0530

    Improved Styling for the app

--- src/PageActions/index.js
+++ src/PageActions/index.js
@@ -9,7 +9,7 @@ export default function PageActions() {
   return (
     <div className="page-actions-container">
       <div className="page-actions">Home</div>
-      <div className="page-actions">File</div>
+      <div className="page-actions page-actions-active">File</div>
       <div className="page-actions">Insert</div>
       <div className="page-actions">Layout</div>
       <div className="page-actions">Help</div>
